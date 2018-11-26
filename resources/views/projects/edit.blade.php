be8d0c6436a4d82869677341342c2d4cbff23278
raul <raul@secret-source.eu>
2018-11-26 20:02:54 +0000

    Episode 12 - Facking PATCH request #comment Added a form and allowing it to get the update method (PATCH) - Using {{method_field('PATCH')}}. Also a layout has been added.

---
+++ resources/views/projects/edit.blade.php
@@ -0,0 +1,28 @@
+@extends('layout')
+@section('content')
+    <h1 class="title">Edit Project</h1>
+    <form method="POST" action="/projects/{{$project->id}}">
+        {{ method_field('PATCH') }}
+        {{ csrf_field() }}
+        <div class="field">
+            <label for="title" class="label">Title</label>
+
+            <div class="control">
+                <input name="title" type="text" class="input" placeholder="Title" value="{{$project->title}}">
+            </div>
+            <div class="field">
+                <label for="description" class="label">Description</label>
+
+                <div class="control">
+                    <textarea name="description" class="textarea">{{$project->description}}</textarea>
+                </div>
+            </div>
+
+            <div class="field">
+                <div class="control">
+                    <button type="submit" class="button is-link">Update Project</button>
+                </div>
+            </div>
+        </div>
+    </form>
+@endsection
