7a817b97d0bf2508ae8de9086c68bd6ef4dcf811
raul <raul@secret-source.eu>
2018-11-27 19:12:18 +0000

    Episode 14 - Cleaner controller and Mass assignment concern #comment ProjectController has been optimized. Added acceptance for fields at model level.

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -12,46 +12,49 @@ class ProjectsController extends Controller
         $projects = Project::all();
         return view('projects.index', compact('projects'));
     }
+
+    public function show(Project $project)
+    {
+        return view('projects.show', compact('project'));
+    }
     
     public function create()
     {
         return view('projects.create');
     }
 
-    public function show()
-    {
-        # code...
-    }
-    public function edit($id)
-    {
-        $project = Project::findOrFail($id);
-        return view('projects.edit', compact('project'));
-    }
-    public function update($id)
-    {
-        $project = Project::findOrFail($id);
-
-        $project->title = request('title');
-        $project->description = request('description');
-
-        $project->save();
-
-        return redirect('/projects');
-    }
-    public function destroy($id)
-    {
-        Project::findOrFail($id)->delete();
-        return redirect('/projects');
-    }
-
     public function store()
     {
+        Project::create(request()->all());
+        
+        return redirect('/projects');
+        /* Project::create([
+            'title' => request('title'),
+            'description' => request('description'),
+        ]);
+
         $project = new Project();
 
         $project->title = request('title');
         $project->description = request('description');
 
-        $project->save();
+        $project->save(); */
+    }
+
+    public function edit(Project $project)
+    {
+        return view('projects.edit', compact('project'));
+    }
+
+    public function update(Project $project)
+    {
+        $project->update(request(['title', 'description']));
+
+        return redirect('/projects');
+    }
+    public function destroy(Project $project)
+    {
+        $project->delete();
         return redirect('/projects');
     }
 }
